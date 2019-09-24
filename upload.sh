d26093abe899a8402b7b25a08c6cf7c38a5f4450
Zeb <elizabeth.delk@colorado.edu>
2019-09-24 15:03:45 -0600

    Restore the ability to see the usage in the upload script.

--- upload.sh
+++ upload.sh
@@ -3,17 +3,17 @@
 cr=`echo $'\n.'`
 cr=${cr%.}
 
-# usage(){
-#   echo
-#   echo "Usage: $0 <application> <rootDir> <baseUrl> <username:password>"
-#   echo
-#   echo "  application     OS or IM - to support differences in APIs (i.e. IM API includes record type, collection or granule)"
-#   echo "  rootDir         The base directory to recursively upload xml files from. For IM this script uses the path of each file to determine type (i.e. 'collections' or 'granules' must be in the path)."
-#   echo "  baseUrl         The target host and context path. The endpoint is built according to the needs to of the application, e.g. for a locally-running IM API: https://localhost:8080/registry"
-#   echo "  username:password  (optional) The username and password for basic auth protected endpoints."
-#   echo
-#   exit 1
-# }
+usage(){
+  echo
+  echo "Usage: $0 <application> <rootDir> <baseUrl> <username:password>"
+  echo
+  echo "  application     OS or IM - to support differences in APIs (i.e. IM API includes record type, collection or granule)"
+  echo "  rootDir         The base directory to recursively upload xml files from. For IM this script uses the path of each file to determine type (i.e. 'collections' or 'granules' must be in the path)."
+  echo "  baseUrl         The target host and context path. The endpoint is built according to the needs to of the application, e.g. for a locally-running IM API: https://localhost:8080/registry"
+  echo "  username:password  (optional) The username and password for basic auth protected endpoints."
+  echo
+  exit 1
+}
 
 postToInventoryManager() { # assumes API_BASE, AUTH are defined, UUID and FILE are read on stdin (use pipe to send file input)
   while read UUID FILE; do
@@ -55,22 +55,27 @@ postItems(){
   done
 }
 
-#args
-APP=$1
-BASEDIR=$2
-MANIFESTS=$(find $BASEDIR -name "manifest.txt")
-API_BASE=$3
-AUTH=$4
+ARGS_COUNT=$#
+if [[ $ARGS_COUNT -eq 3 || $ARGS_COUNT -eq 4 ]]; then
+  #args
+  APP=$1
+  BASEDIR=$2
+  MANIFESTS=$(find $BASEDIR -name "manifest.txt")
+  API_BASE=$3
+  AUTH=$4
 
-# display configuration settings
-echo $cr
-echo "Working config - confirm before proceeding."
-echo "APP - $APP"
-echo "BASEDIR - $BASEDIR"
-echo "MANIFEST FILES - $MANIFESTS"
-echo "API_BASE - $API_BASE"
-echo "AUTH - $AUTH"
-echo $cr
+  # display configuration settings
+  echo $cr
+  echo "Working config - confirm before proceeding."
+  echo "APP - $APP"
+  echo "BASEDIR - $BASEDIR"
+  echo "MANIFEST FILES - $MANIFESTS"
+  echo "API_BASE - $API_BASE"
+  echo "AUTH - $AUTH"
+  echo $cr
 
-# upload
-echo $MANIFESTS | xargs -n 1 | postItems
+  # upload
+  echo $MANIFESTS | xargs -n 1 | postItems
+else
+  usage
+fi
