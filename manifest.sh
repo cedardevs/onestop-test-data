30dc3ef42be651cbbde3127896509cddbdf0e028
StrayCat1 <christopher.esterlein@colorado.edu>
2020-04-06 08:56:30 -0600

    organize granules under collection in manifest

--- manifest.sh
+++ manifest.sh
@@ -16,12 +16,28 @@ getFiles() { # BASEDIR is read on stdin (use pipe to send input)
 
 genManifestContent() { # pipe in list of files to generate uuids for
   while read FILE ; do
-    TYPE="collection"
-    UUID=$(uuidgen | awk '{print tolower($0)}')
-    if [[ $FILE = *"granules"* ]]; then
-      TYPE="granule"
+    if [[ $FILE = *"collection"* ]]; then
+      IDENTIFIER=$(grep fileIdentifier $FILE -A1 | grep CharacterString  | cut -d'>' -f 2 |  cut -d '<'  -f 1)
+      if ! grep -Fq "$FILE" "$MANIFEST"
+      then
+        UUID=$(uuidgen | awk '{print tolower($0)}')
+        echo "$UUID $FILE"
+      else UUID=$(grep $FILE $MANIFEST | cut -d ' ' -f1 ) #get existing collection  uuid
+      fi
+      gatherGranules
     fi
-    echo "$UUID $FILE"
+  done
+}
+
+gatherGranules() {
+  GRANULE_FILES=$(grep -rlE "($IDENTIFIER|$UUID)" $BASEDIR/granules)
+  for GRANULE in $GRANULE_FILES
+  do
+    if ! grep -Fq "$GRANULE" "$MANIFEST"
+    then
+      GRANULE_UUID=$(uuidgen | awk '{print tolower($0)}')
+      printf '%s\n' /^$UUID/a "$GRANULE_UUID $GRANULE" . w q | ex -s $MANIFEST #insert granule after collection
+    fi
   done
 }
 
@@ -32,13 +48,8 @@ updateManifest() {
       echo $BASEDIR | getFiles | genManifestContent >> $MANIFEST
       >&2 echo "Created manifest $MANIFEST"
     else
-      echo $BASEDIR | getFiles | while read FILE; do
-        if ! grep -Fq "$FILE" "$MANIFEST"
-        then
-          >&2 echo "Adding $FILE to $MANIFEST"
-          echo $FILE | genManifestContent >> $MANIFEST
-        fi
-      done
+      echo $BASEDIR | getFiles | genManifestContent >> $MANIFEST
+      >&2 echo "Updated manifest $MANIFEST"
     fi
     echo $MANIFEST | sortManifest
   done
@@ -49,11 +60,11 @@ sortManifest() {
     TMP=$MANIFEST.tmp
     cat $MANIFEST | while read uuid file; do
       if [[ -f "$file" ]]; then
-        echo $file $uuid
+        echo $file $uuid >> $TMP
       else
         >&2 echo "Removing $file from $MANIFEST"
       fi
-    done | sort > $TMP
+    done
     rm $MANIFEST
     cat $TMP | while read file uuid; do
       echo $uuid $file
